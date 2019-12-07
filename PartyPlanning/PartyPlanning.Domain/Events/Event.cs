18673e6c622db66f7b66879cb2a14c6ff5f44162
ambsenyestudi <adrifre7@hotmail.com>
2019-12-07 08:17:18 +0100

    Event created

---
+++ PartyPlanning/PartyPlanning.Domain/Events/Event.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PartyPlanning.Domain.Events
+{
+    public class Event
+    {
+        public DateTime Date { get; set; }
+    }
+}
