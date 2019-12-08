c5ae69bb359ef47de86a5d3c0104cbbe308fc3ad
ambsenyestudi <adrifre7@hotmail.com>
2019-12-08 11:58:05 +0100

    Logger update

---
+++ PartyPlanning/NugetUsage.md
@@ -0,0 +1,5 @@
+#Serilog
+Used this tutorial
+https://github.com/serilog/serilog-aspnetcore
+https://carlos.mendible.com/2016/09/19/step-step-serilog-asp-net-core/
+https://itnext.io/loggly-in-asp-net-core-using-serilog-dc0e2c7d52eb
---
+++ PartyPlanning/PartyPlanning.API/Controllers/EventsController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using System;
+using System.Collections.Generic;
+
+namespace PartyPlanning.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventsController : ControllerBase
+    {
+        private readonly ILogger<EventsController> logger;
+
+        public EventsController(ILogger<EventsController> logger)
+        {
+            this.logger = logger;
+        }
+        // GET: api/Events
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            logger.LogDebug($"Events gotten at {DateTime.UtcNow.ToShortTimeString()}");
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Events/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Events
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Events/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
